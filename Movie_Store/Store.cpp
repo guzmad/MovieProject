d3f826a5042be155a48695cdd4b9f1e615a6eef1
guzmad <guzmad@uw.edu>
2018-03-05 21:03:30 -0800

    Added the Store Class

---
+++ Movie_Store/Store.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "Store.h"
+
+
+Store::Store()
+{
+
+}
+
+Store::~Store()
+{
+}
+
+void Store::readFile(string fileName)
+{
+
+
+}
+
+void Store::populateMovie(string fileName)
+{
+
+}
+
+void Store::populateCustomer(string customerInfo)
+{
+}
+
+void Store::commandHandler(char command)
+{
+}
+
+void Store::printCustomerHistory(Customer * customer)
+{
+}
+
+void Store::printInventory(MovieList * movieList)
+{
+}
+
+void Store::borrowMovie(Customer * customer)
+{
+}
+
+void Store::returnMovie(Customer * customer)
+{
+}
---
+++ Movie_Store/Store.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef STORE_H
+#define STORE_H
+
+#include "CustomerList.h"
+#include "MovieList.h"
+
+class Store
+{
+
+public:
+	Store();
+	~Store();
+
+	void readFile(string fileName);
+	void populateMovie(string fileName);
+	void populateCustomer(string customerInfo);
+	void commandHandler(char command);
+	void printCustomerHistory(Customer * customer);
+	void printInventory(MovieList * movieList);
+	void borrowMovie(Customer * customer);
+	void returnMovie(Customer * customer);
+
+private:
+
+	CustomerList customerList;
+	MovieList movieList;
+};
+
+#endif // !STORE_
