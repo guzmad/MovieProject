48013bb5cfa4b32d5151388693a180d50b3ac67d
Nathanael Fixx <nathanaelfixx@gmail.com>
2018-03-06 23:52:27 -0800

    Fixed sorting

--- Movie_Store/MovieList.cpp
+++ Movie_Store/MovieList.cpp
@@ -39,7 +39,7 @@ bool MovieList::addMovie(MovieInventory movieInventory)
 	}
 
 	for (int i = 0; i < movieList[element].size(); i++) {
-		if (movieList[element][i].getMovie()->operator==(*movieInventory.getMovie())) {
+		if (*movieList[element][i].getMovie() == (*movieInventory.getMovie())) {
 			changeQuantity(movieList[element][i], movieInventory.getQuantity());
 			return false;
 		}
@@ -105,7 +105,7 @@ int MovieList::getWall(vector<vector<MovieInventory>>& movieList, int left, int 
 	int i = (left - 1);
 
 	for (int j = left; j <= right - 1; j++) {
-		if (movieList[element][j].getMovie() < pivot.getMovie()) {  // this is where the problem is 
+		if (*movieList[element][j].getMovie() < *pivot.getMovie()) {  // this is where the problem is 
 			i++;
 			temp = movieList[element][j];
 			movieList[element][j] = movieList[element][i];
